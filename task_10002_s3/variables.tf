cbec2718c604cc0ea486ac4251094de4e1f04eb1
njt0 <nicat.hummetov1@gmail.com>
2023-04-18 18:50:44 +0000

    S3-Static-Website

---
+++ task_10002_s3/variables.tf
@@ -0,0 +1,13 @@
+# required for AWS
+variable "AKIAVA4SAKYBVGMLWQX4access_key" {}
+variable "/KRTMSsNBjsAoUB836XEfaeVH0iHzvGWaa92K3kX" {}
+variable "region" {
+    default = "us-east-1"
+}
+variable "root_domain" {
+    default = "blog_njt_domain"
+}
+
+variable "blog_bucket_subdomain" {
+    default = "demo-subdomain"
+}
